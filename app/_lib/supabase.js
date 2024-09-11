3fc60a89ade07ab7049297c216b21c733901db0d
SabinaCristea <sabinacristea997@gmail.com>
2024-09-11 15:18:24 +0300

    Fetching cabins

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,5 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+// export const supabase = createClient(
+//   "https://usioiuiauydnbpfwfawx.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzaW9pdWlhdXlkbmJwZndmYXd4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNjAxMzE3OSwiZXhwIjoyMDIxNTg5MTc5fQ.lndbWU9rmZjE5k27PJfE6g3q_tNfFhKst9AtJRRqJ8c"
+// );
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,10 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export default async function Page() {
+  console.log("Starting...");
+  const cabins = await getCabins();
+  console.log(cabins);
 
   return (
     <div>
