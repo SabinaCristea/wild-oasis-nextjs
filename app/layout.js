ff6eb079ef23e0aad07299f9e3874b00306112cd
SabinaCristea <sabinacristea997@gmail.com>
2024-09-11 10:34:09 +0300

    Improved navigation and root layout, optimized image

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ const josefin = Josefin_Sans({
 console.log(josefin);
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -26,14 +27,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
