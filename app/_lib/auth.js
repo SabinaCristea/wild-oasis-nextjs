113785452e62e062dcca6f0e5fbf894d0bc77648
SabinaCristea <sabinacristea997@gmail.com>
2024-10-15 18:40:48 +0300

    Implemented middleware

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -9,36 +9,14 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
-  //   callbacks: {
-  //     authorized({ auth, request }) {
-  //       return !!auth?.user;
-  //     },
-  //     async signIn({ user, account, profile }) {
-  //       try {
-  //         const existingGuest = await getGuest(user.email);
-
-  //         if (!existingGuest)
-  //           await createGuest({ email: user.email, fullName: user.name });
-
-  //         return true;
-  //       } catch {
-  //         return false;
-  //       }
-  //     },
-  //     async session({ session, user }) {
-  //       const guest = await getGuest(session.user.email);
-  //       session.user.guestId = guest.id;
-  //       return session;
-  //     },
-  //   },
-  //   pages: {
-  //     signIn: "/login",
-  //   },
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user; //returns boolean
+    },
+  },
 };
 
 export const {
   auth,
-  // signIn,
-  // signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
